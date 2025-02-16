e359a10cb3a71237793b30b310156f49334ad4b6
SalinasJason <jasonsalinas770@gmail.com>
2025-02-15 16:23:57 -0800

    Created a dervied class

---
+++ COMP003A.LectureActivity6-1/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.LectureActivity6_1
+{
+    /// <summary>
+    /// Represents a car, which is a type of vehicle
+    /// </summary>
+    internal class Car : Vehicle
+    {
+        // Auto-implemented properties
+        public int Doors {  get; set; }
+
+        /// <summary>
+        /// Constructs a car with the specified brand, year and number of doors.
+        /// </summary>
+        /// <param name="brand"></param>
+        /// <param name="year"></param>
+        /// <param name="doors"></param>
+        public Car(string brand, int year, int doors)
+        {
+            Brand = brand;
+            Year = year;
+            Doors = doors;
+        }
+
+        /// <summary>
+        /// Displays car details including inherited properties.
+        /// </summary>
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Doors: {Doors}");
+        }
+    }
+}
